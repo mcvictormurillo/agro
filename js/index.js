5986d9f22fb3631a2bebf2f30c4fdc6bf5004e6c
VICTOR MANUEL MURILLO CAMAYO <mcvictor@unicauca.edu.co>
2021-08-19 22:29:05 -0500

    feat: test complete

--- js/index.js
+++ js/index.js
@@ -6,13 +6,19 @@ var app = new Vue({
 		files: "",
 	},
 	methods: {
-		handleFileUploads() {
+		handleFile() {
 			this.files = this.$refs.files.files;
-			const file = this.files[0];
-			console.log("file", file);
-			setTimeout(function () {
-				alert(file.name);
-			}, 3000);
+		},
+		showFileName() {
+			if (this.files.length) {
+				const file = this.files[0];
+				console.log("file", file);
+				setTimeout(function () {
+					alert(file.name);
+				}, 3000);
+			} else {
+				alert("Selecciona un archivo...");
+			}
 		},
 	},
 });
